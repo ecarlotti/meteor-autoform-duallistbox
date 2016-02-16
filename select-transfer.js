2868f7453c164d9ec3f5f4b459d4ee3b4c8827e8
Oli Oskarsson <oli@oskarsson.nl>
2016-02-16 11:06:40 +0100

    Added refresh behaviour when context changes

--- select-transfer.js
+++ select-transfer.js
@@ -1,59 +1,64 @@
 AutoForm.addInputType("select-transfer", {
-  template: "afSelectTransfer",
-  valueIsArray: true,
-  valueOut: function () {
-    return AutoForm.Utility.getSelectValues(this[0]);
-  },
-  contextAdjust: function (context) {
-    // build items list
-    context.items = _.map(context.selectOptions, function(opt) {
-      if (opt.optgroup) {
-        var subItems = _.map(opt.options, function(subOpt) {
-          return {
-            name: context.name,
-            label: subOpt.label,
-            value: subOpt.value,
-            htmlAtts: _.omit(subOpt, 'label', 'value'),
-            // _id must be included because it is a special property that
-            // #each uses to track unique list items when adding and removing them
-            // See https://github.com/meteor/meteor/issues/2174
-            _id: subOpt.value,
-            selected: _.contains(context.value, subOpt.value),
-            atts: context.atts
-          };
+    template: "afSelectTransfer",
+    valueIsArray: true,
+    valueOut: function () {
+        return AutoForm.Utility.getSelectValues(this[0]);
+    },
+    contextAdjust: function (context) {
+        // build items list
+        context.items = _.map(context.selectOptions, function (opt) {
+            if (opt.optgroup) {
+                var subItems = _.map(opt.options, function (subOpt) {
+                    return {
+                        name: context.name,
+                        label: subOpt.label,
+                        value: subOpt.value,
+                        htmlAtts: _.omit(subOpt, 'label', 'value'),
+                        // _id must be included because it is a special property that
+                        // #each uses to track unique list items when adding and removing them
+                        // See https://github.com/meteor/meteor/issues/2174
+                        _id: subOpt.value,
+                        selected: _.contains(context.value, subOpt.value),
+                        atts: context.atts
+                    };
+                });
+                return {
+                    optgroup: opt.optgroup,
+                    items: subItems
+                };
+            } else {
+                return {
+                    name: context.name,
+                    label: opt.label,
+                    value: opt.value,
+                    htmlAtts: _.omit(opt, 'label', 'value'),
+                    // _id must be included because it is a special property that
+                    // #each uses to track unique list items when adding and removing them
+                    // See https://github.com/meteor/meteor/issues/2174
+                    _id: opt.value,
+                    selected: _.contains(context.value, opt.value),
+                    atts: context.atts
+                };
+            }
         });
-        return {
-          optgroup: opt.optgroup,
-          items: subItems
-        };
-      } else {
-        return {
-          name: context.name,
-          label: opt.label,
-          value: opt.value,
-          htmlAtts: _.omit(opt, 'label', 'value'),
-          // _id must be included because it is a special property that
-          // #each uses to track unique list items when adding and removing them
-          // See https://github.com/meteor/meteor/issues/2174
-          _id: opt.value,
-          selected: _.contains(context.value, opt.value),
-          atts: context.atts
-        };
-      }
-    });
 
-    return context;
-  }
+        Tracker.afterFlush(function() {
+            this.$('select').bootstrapDualListbox('refresh');
+        });
+
+        return context;
+    }
 });
 
-Template.afSelectTransfer.rendered = function () {
-  this.$('select').bootstrapDualListbox(template.data.atts.bootstrapDualListboxOptions || {});
-};
+Template.afSelectTransfer.onRendered(function () {
+    this.$('select').bootstrapDualListbox(this.data.atts.bootstrapDualListboxOptions || {});
+});
 
-Template.afSelectTransfer.destroyed = function () {
-  try {
-    if (this.view && this.view._domrange) {
-      this.$('select').bootstrapDualListbox('destroy');
+Template.afSelectTransfer.onDestroyed(function () {
+    try {
+        if (this.view && this.view._domrange) {
+            this.$('select').bootstrapDualListbox('destroy');
+        }
+    } catch (error) {
     }
-  } catch (error) {}
-};
+});
